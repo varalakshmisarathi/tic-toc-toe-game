b5d15dc505837e5b19ec3d09779b1a59098caaaf
varalakshmisarathi <37236117+varalakshmisarathi@users.noreply.github.com>
2020-07-06 14:12:36 +0000

    Create index.js

---
+++ tic toc toe task/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./Game";
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
